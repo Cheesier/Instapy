cbd3367872b6b97fbb9e311bac937d9bc9027027
Adam Nyberg <adam.nyberg@gmail.com>
2012-11-06 11:54:26 +0100

    added a class
    Signed-off-by: Adam Nyberg <adam.nyberg@gmail.com>

--- src/MainInstagram.py
+++ src/MainInstagram.py
@@ -1,4 +1,22 @@
 print "This is the first line of our Instagram application"
 import Image
-im = Image.open("../pic/insta01.jpg")
-im.rotate(45).show()
+import ImageFilter
+#im = Image.open("../pic/girl.jpg")
+#im.filter(ImageFilter.BLUR).rotate(45).show()
+
+class CImageInstagram:
+    def __init__(self, filename=None):
+        self.name = filename
+        self.im = Image.open(filename)
+    def setDecription(self, description=None):
+        self.desc = description
+    def printDescription(self):
+        print self.desc
+    def setFilter(self, filter=None):
+        self.im = self.im.filter(filter)
+        self.im.show()
+        
+c = CImageInstagram("../pic/insta01.jpg")
+c.setDecription("This is a comment to this image")
+c.printDescription()
+c.setFilter(ImageFilter.EMBOSS)
