1e229af5d5e0c363273859b6b2a0ab7671175b4a
Oscar Thunberg <ostenip@gmail.com>
2012-11-06 15:25:00 +0100

    .pyc files gone

--- src/Filters/CFilterVignette.py
+++ src/Filters/CFilterVignette.py
@@ -5,28 +5,17 @@ import math
 class CFilterVignette(CFilter):
     def applyFilter(self, aImage):
         def distanceFromCenter(self, pos):
-            return math.sqrt((pos[0]-aImage.size[0]) + (pos[1]-aImage.size[1]))
+            return math.sqrt((pos[0]-aImage.size[0]/2) + (pos[1]-aImage.size[1]/2))
         
         # intialize a new filtered image
         new_image = Image.new('RGB', aImage.size)
         new_image_list = []
         #get the pixels list from the original image
         pixels = aImage.getdata()
-        """
-        for pixel in pixels:
-            r = int(pixel[0]);
-            g = int(pixel[0]);
-            b = int(pixel[0]);
-            # human eye is bad at seeing red and blue, 
-            # so we de-emphasize them
-            v = int(0.2126*r + 0.7152*g + 0.0722*b);
-            new_pixel = (v,v,v)
-            new_image_list.append(new_pixel)
-        """
         #traverse the pixel list and extract the R. G and B 
         #components for each pixel
-        for x in range(aImage.size[1]):
-            for y in range(aImage.size[0]):
+        for x in range(aImage.size[0]):
+            for y in range(aImage.size[1]):
                 r = int(pixels[x][y][0]);
                 g = int(pixels[x][y][0]);
                 b = int(pixels[x][y][0]);
