811f858569c3cbbf2d126b724026929aae491210
Adam Nyberg <adam.nyberg@gmail.com>
2012-11-06 16:54:38 +0100

    massa jävla skit
    Signed-off-by: Adam Nyberg <adam.nyberg@gmail.com>

--- src/Lib.py
+++ src/Lib.py
@@ -1,8 +1,15 @@
 import Image
 
 def to2d(image):
+    pix = image.getdata()
+    size = image.size
+    
+    sizeX = size[0]
     data = []
-    pix = image.load()
-    for y in xrange(image.size[1]):
-        data.append([pix[x, y] for x in xrange(image.size[0])])
+    temp = []
+    for i in range(len(pix)):
+        temp.append(pix[i])
+        if (i != 0 and i % sizeX == 0) or i == len(pix)-1:
+            data.append([temp])
+            temp = []
     return data
