10e36d831023f7a9552af049f8b9fe8f5451aee4
Oscar Thunberg <ostenip@gmail.com>
2012-11-09 10:41:34 +0100

    Added working Slideshow

--- src/Slideshow.py
+++ src/Slideshow.py
@@ -8,42 +8,57 @@ from Filters.CFilterColor import *
 from Filters.CFilterPrime import *
 from Filters.CFilterPlainBorder import *
 from Lib import *
+import threading
 import os
 import random
 
 filters = [
            CFilterVignette(),
            CFilterGrayscale(),
-           #CFilterColor(),
-           #CFilterPrime(),
+           CFilterColor((1,0,0)),
+           CFilterPrime(),
            CFilterPlainBorder(),
            CFilterBlur(),
            ]
 
-# Remove old images
-oldPics = os.listdir("../slideshow")
-for image in range(len(oldPics)):
-    removeImg("../slideshow/" + oldPics[image])
-
 # Add new filtered images
 pics = os.listdir("../pic")
+"""
 for image in range(len(pics)):
     c = CImageInstagram("../pic/" + pics[image])
     c.applyFilter(random.sample(filters, 1))
     c.save("../slideshow/" + pics[image])
+"""
+global current
+current = -1
+
+class applyFilterLoop(threading.Thread):
+    def run(self):
+        p = 0
+        while True:
+            global current
+            current = p
+            c = CImageInstagram("../pic/" + pics[p])
+            c.applyFilter(random.sample(filters, 1))
+            c.save("../slideshow/" + pics[p])
+            p = (p+1)%len(pics)
+
+filterLoop = applyFilterLoop()
+filterLoop.start()
 
 lastShown = time.time()
 n = 0
 
 # Show slideshow
-newPics = os.listdir("../pic")
+newPics = os.listdir("../slideshow")
 while True:
-    c = CImageInstagram("../pic/" + newPics[n])
-    c.showImage()
-    
-    timeNow = time.time()
-    print "lastShown:", timeNow-lastShown
-    lastShown = timeNow
-    
-    time.sleep(1)
+    if n != current:
+        c = CImageInstagram("../slideshow/" + newPics[n])
+        c.showImage()
+        
+        timeNow = time.time()
+        print "lastShown:", timeNow-lastShown
+        lastShown = timeNow
+        
+        time.sleep(4.6)
     n = (n+1)%len(newPics)
