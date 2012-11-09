c031278533cdde19fcfb25295553a9d2ad111a57
Oscar Thunberg <ostenip@gmail.com>
2012-11-09 09:23:39 +0100

    Filters optimized, added delete file method
    Huge improvement for the prime filter, going from 25s down to 4s (on my
    machine). Also did some improvement on some other filters.

--- src/Filters/CFilterBrigthness.py
+++ src/Filters/CFilterBrigthness.py
@@ -18,18 +18,11 @@ class CFilterBrigthness(CFilter):
         # darkened version to the new image list
             
         for pixel in pixels:
-            new_pixel = (int(pixel[0] * self.brightness), \
-                         int(pixel[1] * self.brightness), \
+            new_pixel = (int(pixel[0] * self.brightness),
+                         int(pixel[1] * self.brightness),
                          int(pixel[2] * self.brightness))
-            # print new_pixel
-            #check the new pixel values are within rgb range
-            for pixel in new_pixel:
-                if pixel > 255:
-                    pixel = 255
-                elif pixel < 0:
-                    pixel = 0
+            
             new_image_list.append(new_pixel)
-        # print new_pixel
-                #aImage.putdata(new_image_list)
+            
         new_image.putdata(new_image_list)
         return new_image
--- src/Filters/CFilterPrime.py
+++ src/Filters/CFilterPrime.py
@@ -12,35 +12,31 @@ class CFilterPrime(CFilter):
         new_image = Image.new('RGB', aImage.size)
         new_image_list = []
         #get the pixels list from the original image
-        #pixels = aImage.getdata()
+        pixels = aImage.getdata()
         
-        pixels = Lib.to2d(aImage)
+        primes = Lib.primes
+        prime = 0
         
-        height = len(pixels)
-        width = len(pixels[0])
-        
-        print height, width
         n = 0
         p = 1
-        for x in range(width):
-            for y in range(height):
+        #for x in range(width):
+            #for y in range(height):
+        for pixel in pixels:
                 
-                r = int(pixels[y][x][0])
-                g = int(pixels[y][x][1])
-                b = int(pixels[y][x][2])
-                
-                if Lib.isprime(n):
-                    r*= int(self.multiply[0])
-                    g*= int(self.multiply[1])
-                    b*= int(self.multiply[2])
-                    p+=1
-                
-                # human eye is bad at seeing red and blue, 
-                # so we de-emphasize them
-                #v = int(r + g + b);
-                new_pixel = (r,g,b)
-                new_image_list.append(new_pixel)
-                n+=1
+            r = int(pixel[0])
+            g = int(pixel[1])
+            b = int(pixel[2])
+            
+            if primes[prime] == n:
+                r*= int(self.multiply[0])
+                g*= int(self.multiply[1])
+                b*= int(self.multiply[2])
+                p+=1
+                prime += 1
+            
+            new_pixel = (r,g,b)
+            new_image_list.append(new_pixel)
+            n+=1
                 
         new_image.putdata(new_image_list)
         print "primes:", n-p, ", out of", n , "pixels"
