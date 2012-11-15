4e5066de4ed006baf9080061e6199f501e99feb7
Adam Nyberg <adam.nyberg@gmail.com>
2012-11-15 14:39:28 +0100

    added ajax
    Signed-off-by: Adam Nyberg <adam.nyberg@gmail.com>

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-	$('add-filter').on('click', function() {
-    	
+	$('.fileinput-button').on('change', function() {
+    	$.ajax({
+    		type: 'POST',
+    		url: '/upload',
+			cache: false,
+			datatype: 'json',
+			success: function(msg){
+				console.log(msg)
+				
+			},
+			error: function(){
+				alert('Nu blev det fel!');
+			}
+		});
+		//showPreview(this);
+		
     });
+    
+	function showPreview(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			
+			reader.onload = function (e) {
+			$('.active-img')
+			.attr('src', e.target.result)
+			.width(150)
+			.height(200);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
 });
