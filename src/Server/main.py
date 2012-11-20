06a279ab5fe9584c92da8670e7d7f77043fd7055
Oscar Thunberg <ostenip@gmail.com>
2012-11-20 15:31:46 +0100

    Filtering added, just a prototype

--- src/Server/main.py
+++ src/Server/main.py
@@ -1,7 +1,20 @@
 from bottle import route, run, request, static_file
 from API.Upload import Upload
 #from API.Filter import Filter
+from src.Filters.CFilterBlur import *
+from src.Filters.CFilterPrime import *
+from src.Filters.CFilterVignette import *
+from src.CImageInstagram import *
 from os import path
+
+# PROTOTYPE
+global filters
+filters = {
+           'blur': CFilterBlur(),
+           'prime': CFilterPrime(),
+           'vignette': CFilterVignette(),
+                  }
+# END PROTOTYPE
 
 APIs = {'upload': Upload(),
         #'filter': Filter(),
@@ -10,13 +23,21 @@ APIs = {'upload': Upload(),
 @route('/upload', method='POST')
 def upload():
     data = request.files.data
-    filter = request.forms.filter
+    filtername = request.forms.filter
     #print "data:", data
     if data and data.file:
         fn = path.basename(data.filename)
-        open('../../public/img/' + fn, 'wb').write(data.file.read())
+        open('../../public/tmp/' + fn, 'wb').write(data.file.read())
         
-        return 'http://localhost:8080/img/'+fn
+        #prototype
+        c = CImageInstagram('../../public/tmp/' + fn)
+        if filtername in filters:
+            c.applyFilter(filters[filtername])
+            c.im_copy.save('../../public/tmp/' + filtername + '_' + fn)
+        #end of proto
+        
+        return {'org': 'http://localhost:8080/tmp/'+fn,
+                'filtered': 'http://localhost:8080/tmp/'+filtername+'_'+fn}
     return "Something went wrong"
 
 @route('/api')
