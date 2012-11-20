725058b7073782dfa129ac1dfe900cbd2efad3fb
Adam Nyberg <adam.nyberg@gmail.com>
2012-11-20 17:07:09 +0100

    new style and added animation
    Signed-off-by: Adam Nyberg <adam.nyberg@gmail.com>

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,55 +3,54 @@
 		formdata = false;
 
 	function showUploadedItem (source) {
-  		var preview = $("#preview-org"),
-	  		img  = document.createElement("img");
-  		img.src = source;
-		preview.append(img);
+		$('<img />', { 'src': source}).appendTo('#preview-org');
 	}   
 
 	if (window.FormData) {
   		formdata = new FormData();
-  		document.getElementById("btn-submit").style.display = "none";
+  		$("#btn-submit").hide();
 	}
 	
- 	input.addEventListener("change", function (evt) {
- 		document.getElementById("response").innerHTML = "Uploading . . ."
+ 	$('input').on('change', function(){
+ 		$('#upload-form').slideUp('slow');
+ 		$('#loading-gif').show();
  		var i = 0, len = this.files.length, img, reader, file;
-	
-		for ( ; i < len; i++ ) {
-			file = this.files[i];
-	
-			if (!!file.type.match(/image.*/)) {
-				if ( window.FileReader ) {
-					reader = new FileReader();
-					reader.onloadend = function (e) { 
-						showUploadedItem(e.target.result, file.fileName);
-					};
-					reader.readAsDataURL(file);
-				}
-				if (formdata) {
-					formdata.append("data", file);
-				}
-			}	
+ 		if(len > 0){
+			for ( ; i < len; i++ ) {
+				file = this.files[i];
+		
+				if (!!file.type.match(/image.*/)) {
+					if ( window.FileReader ) {
+						reader = new FileReader();
+						reader.onloadend = function (e) { 
+							showUploadedItem(e.target.result, file.fileName);
+						};
+						reader.readAsDataURL(file);
+					}
+					if (formdata) {
+						formdata.append("data", file);
+					}
+				}	
+			}
+		
+			if (formdata) {
+				formdata.append('filter', $('#filter-select').val());
+				$.ajax({
+					url: "/upload",
+					type: "POST",
+					data: formdata,
+					processData: false,
+					contentType: false,
+					success: function (data) {
+						$('#image-list').remove();
+						$('#response').html('');
+						$('<img />', { 'src': data.filtered}).appendTo('#response');
+					},
+					error: function (res) {
+						$('#response').html('Server Error'); 
+					}
+				});
+			}
 		}
-	
-		if (formdata) {
-			formdata.append('filter', $('#filter-select').val());
-			$.ajax({
-				url: "/upload",
-				type: "POST",
-				data: formdata,
-				processData: false,
-				contentType: false,
-				success: function (data) {
-					$('#image-list').remove();
-					$('#response').html('');
-					$('<img />', { 'src': data.filtered}).appendTo('#response');
-				},
-				error: function (res) {
-					$('#response').html('Server Error'); 
-				}
-			});
-		}
-	}, false);
+	});
 }());
